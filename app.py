d4e961789f2b4912c96cad76a564ebf786b2de67
Zarzuke <zankiff2013@gmail.com>
2024-07-20 20:48:27 -0400

    hola
    holas

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ print("hola mundo")
 print("Vuelve mi vida, te extraño")
 print("por favor, regresa a mi, no puedo mas")
 print("andres le gusta la barba de Arthur")
+
+print("hola mundo")
+print("la vida es beia")
