f04bcbdc1a81fc924d8dff23415f579999ded5dd
Zarzuke <zankiff2013@gmail.com>
2024-07-20 20:44:05 -0400

    cesar
    cesart

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 print("hola mundo")
-<<<<<<< HEAD
 print("Vuelve mi vida, te extraño")
-=======
-
+print("cesar pa cuando el wolverine")
 print("andres le gusta la barba de Arthur")
->>>>>>> 0328c7c1b64313cc7959f978b036cb81456b9fc9
