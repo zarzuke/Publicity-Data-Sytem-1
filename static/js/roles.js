fc1bec3ccf432290f1862035b639de9d30c46284
Zarzuke <92259266+zarzuke@users.noreply.github.com>
2024-08-28 11:45:08 -0400

    url diseño corregida

--- static/js/roles.js
+++ static/js/roles.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const entrega = document.getElementById('entrega');
     const client = document.getElementById('client')
     const home = document.getElementById('home')
+    const setting = document.getElementById('setting')
   
     if (role === 'Designer') {
         client.style.display = 'none';
@@ -15,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
         elaboracion.style.display = 'none';
         entrega.style.display = 'none';
         home.style.display = 'none';
+        setting.style.display = 'none';
     } else if (role === 'Administrator') {
         cdiseño.style.display = 'block';
         aprobacion.style.display = 'block';
--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -83,33 +83,4 @@ closeButton.addEventListener('click', () => {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  const role = document.getElementById('role').textContent.trim();
-  const clientLink = document.getElementById('client');
-  const diseño = document.getElementById('diseño');
-  const aprobacion = document.getElementById('aprobacion');
-  const elaboracion = document.getElementById('elaboracion');
-  const entrega = document.getElementById('entrega');
-  const client = document.getElementById('client')
-  const home = document.getElementById('home')
-
-  if (role === 'Designer') {
-      client.style.display = 'none';
-      diseño.style.display = 'block';
-      aprobacion.style.display = 'none';
-      elaboracion.style.display = 'none';
-      entrega.style.display = 'none';
-      home.style.display = 'none';
-  } else if (role === 'Administrator') {
-      cdiseño.style.display = 'block';
-      aprobacion.style.display = 'block';
-      elaboracion.style.display = 'block';
-      entrega.style.display = 'block';
-  }
-});
-
-// ...
-
-
-
 
